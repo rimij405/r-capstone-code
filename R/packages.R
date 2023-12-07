c4ba309a3d4ce66947536429b00092696d5404a8
Ian Effendi <effendiian@gmail.com>
2023-12-06 22:44:38 -0500

    chore(package): :pushpin: Create `renv::snapshot()` of latest dependencies.

---
+++ R/packages.R
@@ -0,0 +1,10 @@
+# Load the required libraries for {targets} workflows.
+library(targets)
+library(tarchetypes)
+library(qs)
+
+# Load the tidyverse packages required for analysis.
+library(here)
+library(tibble)
+library(tidyr)
+library(zipcodeR)
