5841aac58ea5caf751ff6d7891b94bdf2b71f01e
Ian Effendi <effendiian@gmail.com>
2023-12-07 12:27:28 -0500

    feat(config): :sparkles: Add module `src/config` for configuring application settings.

---
+++ src/config.R
@@ -0,0 +1,16 @@
+#' @family src/config
+#' @name config[app]
+#'
+#' @title app()
+#'
+#' @description
+#' Get the application configuration as a list.
+#'
+#' @return List containing specific configuration details.
+#' @export
+app <- function() {
+    return(list(
+        MY_ORCID = Sys.getenv("ORCID", "XXXX-XXXX-XXXX-XXXX"),
+        GRAPH_TITLE = Sys.getenv("VISNETWORK_GRAPH_TITLE", "graph")
+    ))
+}
